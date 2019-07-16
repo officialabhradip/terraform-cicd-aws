a97bf3491a95d715a9646de50a675be0eef68107
officialabhradip <52069187+officialabhradip@users.noreply.github.com>
2019-07-16 12:26:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,36 @@
 provider "aws {
-
+access_key = ""
+secret_key = ""
+region = ""
 }
 
 resource "aws_instance" "cicdvm" {
-
+ami = ""
+instance_type = ""
+root_block_device{
+volume_size = "50"
+}
 provisioner "remote-exec" {
     inline = [
-      "puppet apply",
-      "consul join ${aws_instance.web.private_ip}",
+        "wget https://apt.puppetlabs.com/pupetlabs-release-pc1-xenial.deb"
+        "sudo dpkg -i pupetlabs-release-pc1-xenial.deb"
+        "sudo apt-get update"
+        "sudo apt-get install -y puppet-agent git"
+        "sudo ln -s /opt/puppetlabs/bin/puppet /usr/bin/puppet"
+        "sudo puppet module install rtyler-jenkins"
+        "sudo puppet module install hubspot-nexus"
+        "sudo puppet module install maestrodev-sonarqube"
+        "wget jenkins.pp"
+        "wget nexus.pp"
+        "wget sonar.pp"
+        "sudo puppet apply jenkins.pp"
+        "sudo apt-get install -y maven"
+        "sudo puppet apply nexus.pp"
+        "sudo puppet apply sonar.pp"
+        "sudo echo 'sonar.embeddedDatabase.port:        9092' >> /usr/local/sonar/conf/sonar.properties"
+        "sudo /etc/init.d/sonar start"
+        "sudo rm -f /etc/maven/settings.xml"
+        "sudo wget settings.xml -O /etc/maven/settings.xml"
     ]
   }
   
